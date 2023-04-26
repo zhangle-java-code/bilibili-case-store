c52127b55a20210654d2e062575d0b845b231f3c
zhangle <zhangle@vtstar.net>
2023-04-26 19:53:24 +0800

    🚀【知识点】实体类基类

---
+++ src/main/java/com/cy/store/entity/BaseEntity.java
@@ -0,0 +1,11 @@
+package com.cy.store.entity;
+import java.io.Serializable;
+
+
+
+//1.项目中许多实体类都会有日志相关的四个属性，所以在创建实体类之前，应先创建这些实体类的基类，将4个日志属性声明在基类中。在com.cy.store.entity包下创建BaseEntity类，作为实体类的基类。
+// https://blog.csdn.net/TRK_YX/article/details/125574495
+/** 实体类的基类 */
+public class BaseEntity implements Serializable {
+
+}
