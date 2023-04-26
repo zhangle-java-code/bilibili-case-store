a7d11bc4a95b5cd221de1b8a351e3b33ad5f06d1
DESKTOP-FKR3N5I\zhangle <Oliver_web@163.com>
2023-04-26 23:27:25 +0800

    数据连接和测试数据库是否连接成功

--- src/test/java/com/cy/store/StoreApplicationTests.java
+++ src/test/java/com/cy/store/StoreApplicationTests.java
@@ -1,13 +1,26 @@
 package com.cy.store;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
 
 @SpringBootTest
 class StoreApplicationTests {
 
+	@Autowired
+	private DataSource dataSource;
+
+	// 验证是否正常加载上下文
 	@Test
 	void contextLoads() {
+	}
+
+	// HikariPool
+	@Test
+	public void getConnection() throws Exception {
+		System.out.println(dataSource.getConnection());
 	}
 
 }
